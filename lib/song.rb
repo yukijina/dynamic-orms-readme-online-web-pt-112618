137689e62e82d46d2034cd5f6b1122b6bd5980e0
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-15 19:06:17 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
 
   def self.column_names
     DB[:conn].results_as_hash = true
-
+    #binding.pry
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
